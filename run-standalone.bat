06b6fbf28ad78b7319f7ee7ed9e3a3c8432801c9
Pradd <pradd.me@gmail.com>
2013-10-21 15:24:05 +0300

    version 1.0

--- run-standalone.bat
+++ run-standalone.bat
@@ -1,1 +1,1 @@
-java -jar target/activemq-cli-sender-0.1-jar-with-dependencies.jar tcp://localhost:61616 JMS/QUEUE.TEST "message text"
+java -jar target/activemq-cli-sender-1.0-jar-with-dependencies.jar tcp://localhost:61616 JMS/QUEUE.TEST "message text"
